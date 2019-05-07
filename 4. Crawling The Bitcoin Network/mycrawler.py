a6af89c1147442b2d88984d58f0667938951532d
willcl-ark <will8clark@gmail.com>
2019-05-07 15:47:05 +0100

    https://youtu.be/oJ6YR654-hE?list=PLQ56Yiu6lEazrw2dpGC_xXF1zLF2mM872&t=497

--- 4. Crawling The Bitcoin Network/mycrawler.py
+++ 4. Crawling The Bitcoin Network/mycrawler.py
@@ -1,5 +1,7 @@
-from lib import handshake, read_msg, serialize_msg, read_varint, read_address
 from io import BytesIO
+from time import time
+
+from lib import BitcoinProtocolError, handshake, read_address, read_msg, read_varint, serialize_msg
 
 
 def read_addr_payload(stream):
@@ -32,8 +34,45 @@ class Connection:
         self.peer_version_payload = None
         self.nodes_discovered = []
 
+    def handle_ping(self, payload):
+        response = serialize_msg(command=b'pong', payload=payload)
+        self.sock.sendall(response)
+
+    def handle_addr(self, payload):
+        # TODO: interpret the payload
+        payload = read_addr_payload(BytesIO('payload'))
+        if len(payload['addresses']) > 1:
+            self.nodes_discovered = [
+                Node(a['ip'], a['port']) for a in payload['addresses']
+            ]
+
+    def handle_msg(self):
+
+        # Handle next message
+        msg = read_msg(self.stream)
+        command = msg['command'].decode()
+        payload = msg['payload']
+        payload_len = len(msg['payload'])
+        print(f'Received a "{command}" containing {payload_len} bytes')
+
+        method_name = f'handle_{command}'
+
+        # Respond to "ping"
+        if command == b'ping':
+            self.handle_ping(payload_len)
+
+        # specially handle peer lists
+        if command == b'addr':
+            self.handle_addr(payload)
+
+    def remain_alive(self):
+        return not self.nodes_discovered
+
     def open(self):
-        # establish connection
+        # Set start time
+        self.start = time()
+
+        # Open TCP connection
         print(f'Connecting to {self.node.ip}')
         self.sock = handshake(self.node.address)
         self.stream = self.sock.makefile('rb')
@@ -41,33 +80,14 @@ class Connection:
         # request peers' peers
         self.sock.sendall(serialize_msg(b'getaddr'))
 
-        # print every gossip message we receive
-        while True:
-            # Handle next message
-            msg = read_msg(self.stream)
-            command = msg['command']
-            payload_len = len(msg['payload'])
-            print(f'Received a "{command}" containing {payload_len} bytes')
-
-            # Respond to "ping"
-            if command == b'ping':
-                response = serialize_msg(command=b'pong', payload=msg['payload'])
-                self.sock.sendall(response)
-                print("Send pong")
-
-            # specially handle peer lists
-            if command == b'addr':
-                # TODO: interpret the payload
-                payload = read_addr_payload(BytesIO(msg['payload']))
-                if len(payload['addresses']) > 1:
-                    self.nodes_discovered = [
-                        Node(a['ip'], a['port']) for a in payload['addresses']
-                    ]
-                    break
-        pass
+        # handle messages until program exits
+        while self.remain_alive():
+            self.handle_msg()
 
     def close(self):
-        pass
+        # clean up socket's file descriptor
+        if self.sock:
+            self.sock.close()
 
 
 class Crawler:
@@ -76,29 +96,28 @@ class Crawler:
         self.nodes = nodes
 
     def crawl(self):
-        pass
 
+        while True:
+            # get next address from addresses and connect
+            node = self.nodes.pop()
 
-def crawler(nodes):
-    while True:
-        # get next address from addresses and connect
-        node = nodes.pop()
+            try:
 
-        try:
+                conn = Connection(node)
+                conn.open()
+            except (OSError, BitcoinProtocolError) as e:
+                print(f'Error: str({e})')
+                continue
+            finally:
+                conn.close()
 
-            conn = Connection(node)
-            conn.open()
-        except Exception as e:
-            print(f'Error: str({e})')
-            continue
-
-        # Handle the results if no exceptions
-        nodes.extend(conn.nodes_discovered)
-        print(f'{conn.node.ip} report version {conn.peer_version_payload}')
+            # Handle the results if no exceptions
+            self.nodes.extend(conn.nodes_discovered)
+            print(f'{conn.node.ip} report version {conn.peer_version_payload}')
 
 
 if __name__ == '__main__':
-
-    nodes = [Node('2.82.223.39', 8333)]
-    crawler(nodes)
-
+    nodes = [Node('2.82.223.39', 8333),
+             Node('172.105.219.38', 8333),
+             Node('34.222.156.12', 8333)]
+    Crawler(nodes).crawl()
